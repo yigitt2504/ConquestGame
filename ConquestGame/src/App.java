5b31f8aeb1076fff629b090db740cdbc03f9bcbf
yigitt2504 <yigittan04@gmail.com>
2025-02-14 17:48:03 +0300

    creating methods and constructors
    I created some essential methods and constructors

--- ConquestGame/src/App.java
+++ ConquestGame/src/App.java
@@ -1,5 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args){
-        System.out.println("Start of project.");
+        Countries england = new Countries("England");
+        Countries france = new Countries("France");
+
+        Provinces london = new Provinces("London", 1000, england);
+        Provinces paris = new Provinces("Paris", 1000, france);
+        Provinces york = new Provinces("York", 1000, england);
+
+
+        england.addProvince(york);
+        england.addProvince(london);
+        france.addProvince(paris);
+
+        List<Countries> countriesList = new ArrayList<>();
+        countriesList.add(england);
+        countriesList.add(france);
+
+        Scanner selecting_country = new Scanner(System.in);
+        System.out.println("Which country do you want to play as?");
+        String selected_country = selecting_country.nextLine();
+        
+        boolean country_found = false;
+        for(Countries country : countriesList){
+            if(selected_country.equalsIgnoreCase(country.getName())){
+                System.out.println("You are now in charge of: " + country.getName());
+
+                System.out.println(country.getName() + " is in control of these provinces: ");
+                for(Provinces p : country.getProvincesOwned()){
+                    System.out.println(p.getProvinceName());
+                }
+                country_found = true;
+                break;
+            }
+        }
+        if(!country_found){
+            System.out.println("This country doesn't exist, please try again!");
+        }
+
+        selecting_country.close();
+        
+
+
+
     }
 }
---
+++ ConquestGame/src/Provinces.java
@@ -0,0 +1,38 @@
+public class Provinces {
+    String province_name;
+    int troops;
+    int gold;
+    Countries owner;
+
+    public Provinces(String province_name, int troops, Countries owner){
+        this.province_name = province_name;
+        this.troops = troops;
+        this.owner = owner;
+    }
+
+    public String getProvinceName(){
+        return province_name;
+    }
+
+    public void trainingTroops(int troops){
+        this.troops += troops;
+        this.gold -= troops * 100;
+    }
+
+    public String attacking(Provinces attacked_province, int troops){
+        if(troops>attacked_province.troops){
+            this.owner.removeProvince(attacked_province);
+            attacked_province.owner = this.owner;
+            this.owner.addProvince(attacked_province);
+            return("The province is conquered!");
+        }
+        else{
+            return("The defenders have succeded!");
+        }
+    }
+
+}
+
+
+
+
